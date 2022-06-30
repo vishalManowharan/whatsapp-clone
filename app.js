888fd59903c6f55a0b3857972b4fd8de484c57a6
vishal <vam9908@gmail.com>
2022-06-30 16:18:44 +0530

    today's changes

---
+++ app.js
@@ -0,0 +1,9 @@
+for(let i=0;i<30;i++) {
+   document.getElementsByClassName('chats')[i].addEventListener("click",add);
+}
+
+function add () {
+    document.getElementsByClassName('chats')[0].classList.add("chatsa");
+}
+
+
